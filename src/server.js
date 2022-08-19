28ae5e46cfcd2e4ba2deba2f1afc12bedaab8b63
Dawid Kocur <dawidkocur@MacBook-Pro-Dawid.local>
2022-08-19 21:36:32 +0200

    add new routes

--- src/server.js
+++ src/server.js
@@ -7,9 +7,37 @@ export const app = express()
 
 app.disable('x-powered-by')
 
+// middlewares that are used for all app
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const router = express.Router()
+app.use('api/', router)
+
+// middlewares this one to a specific controller
+const log = (request, response, next) => {
+    console.log('logging')
+    next()
+}
+//
+
+router.get('/lol', (request, response) => {
+    response.send({ message: 'hello world' })
+})
+
+app.get('/dlsa', log, (request, response) => {
+    response.send({ message: 'hello' })
+})
+
+app.post('/', (request, response) => {
+    console.log(request.body)
+    response.send({ message: 'ok' })
+})
+
+export const start = () => {
+    app.listen(4000, () => {
+        console.log('server is on port 4000')
+    })
+}
